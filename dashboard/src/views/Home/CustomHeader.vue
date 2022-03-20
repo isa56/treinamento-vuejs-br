5c97ede69b1a83f47e7d8963ecd496d132fce87b
isa56 <isadoraadriana@gmail.com>
2022-03-20 20:40:09 -0300

    Adicionando design tela inicial

---
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+    components: {},
+    setup() {
+        
+
+        return {
+            
+        }
+    },
+
+}
+</script>
+
+
+<template>
+
+    <h1>Meu header</h1>
+
+</template>
+
+
+<style scoped>
+
+
+</style>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,26 +1,54 @@
 /* eslint-disable */
 <script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
 
 export default {
-    name: '',
-    data() {
-        return {
-            
+    components: { CustomHeader, Contact },
+
+    setup() {
+
+        const router = useRouter
+
+        function handleLogin() {
+
         }
-    },
+
+        function handleAccountCreate() {
+
+        }
+
+        onMounted(() => {
+            const token = window.localStorage.getItem('token')
+            if (token) {
+                router.push({ name: 'Feedbacks' })
+            }
+        })
+
+        return {
+            handleLogin,
+            handleAccountCreate
+        }
+
+    }
 
 }
 </script>
 
 
 <template>
+    <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
 
-    <h1 class="font-black text-brand-main">Aqui é a Home</h1>
+    <contact />
 
+    <div class="flex justify-center py-10 bg-brand-gray">
+        <p class="font-medium text-center text-gray-800">Feedbacker © 2022</p>
+    </div>
 </template>
 
 
 <style scoped>
-
-
 </style>
